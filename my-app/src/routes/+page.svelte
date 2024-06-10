938f91744407a1d1bb3a66c9d3a02ed2bbdd7766
Jaime Amigo <jamigo@mail.utdt.edu>
2024-06-10 20:07:34 -0300

    first commit

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+
+<head>
+    <link rel="stylesheet" href="./src/style/styles.css">
+</head>
+
+<div style="height: 150px;"></div>
+<h1 class ='title'>La historia a 300km/h</h1>
+<p class = 'subtitle'>La evolución de la Formula 1 contada a través de los datos</p>
+<div class = "separator"></div>
+<div class = "history-container">
+    <div class = "intro-div">
+        <p class = 'body'>La Fórmula 1, comúnmente conocida como F1, es la categoría reina del automovilismo, donde los mejores pilotos y las más avanzadas tecnologías de vehículos se enfrentan en circuitos de todo el mundo. Este deporte de alta velocidad y precisión es un espectáculo de ingeniería y habilidad humana, que atrae a millones de aficionados cada temporada.</p>
+        <p class = 'body'>La Fórmula 1 es una competición de monoplazas, vehículos diseñados específicamente para este tipo de carreras, que se destacan por su aerodinámica sofisticada y motores potentes. Los coches de F1 pueden alcanzar velocidades superiores a los 350 km/h, con aceleraciones que desafían los límites físicos tanto de la máquina como del piloto.</p>
+        <p class = 'body'>La historia de la Fórmula 1 comenzó en 1950, cuando se celebró el primer Campeonato Mundial de Pilotos. La carrera inaugural tuvo lugar en el circuito de Silverstone, en el Reino Unido. Desde entonces, la F1 ha evolucionado significativamente, pasando de ser una serie de carreras entre amigos y entusiastas a una competición global con equipos multimillonarios, tecnología punta y una enorme audiencia internacional. Grandes nombres como Juan Manuel Fangio, Ayrton Senna, Michael Schumacher y Lewis Hamilton han dejado su huella en este deporte, cada uno marcando épocas de dominio y redefiniendo lo que significa ser el mejor en la pista.</p>
+    </div>
+    <div class = "image-container">
+        <img class = "image" src="../src/assets/images/first_GP.jpg" alt="First GP">    
+        <img class = "image" src="../src/assets/images/first_GP2.jpg" alt="First GP2">
+    </div>
+</div>
+<div class = "separator"></div>
+<div>
+    <img class = "timeline" src="../src/assets/images/timeline.jpg" alt="F1 Timeline">
+</div> 
