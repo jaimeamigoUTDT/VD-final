79486d435ff4724df8caa34899b574513be5389f
Jaime Amigo <jamigo@mail.utdt.edu>
2024-06-19 13:56:18 -0300

    csv plugin

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,6 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dsv from '@rollup/plugin-dsv';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit(), dsv({
+        processRow: (row, id) => {
+          Object.keys(row).forEach((key) => {
+            var value = row[key];
+            row[key] = isNaN(+value) ? value : +value;
+          });
+        }
+      })]
 });
